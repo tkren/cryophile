642f684c745d00b6872c1b23b7fc76d9f02b2007
Thomas Krennwallner <tk@postsubmeta.net>
2022-03-06 20:11:34 -0500

    backup: use spool dir and uuid vault dir

--- src/backup.rs
+++ src/backup.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Utc};
+
 use crate::constants::DEFAULT_COMPRESSION;
 use crate::Config;
 use crate::FinalEncoder;
@@ -14,28 +16,51 @@ pub fn perform_backup(config: Config, matches: &clap::ArgMatches) -> io::Result<
         log::debug!("Printing normally...");
     }
 
-    let input = matches.value_of("input").unwrap_or("-");
+    let mut backup_dir = PathBuf::new();
 
-    let reader: Box<dyn io::Read> = match input {
-        "-" => {
-            log::info!("Reading from stdin ...");
-            Box::new(io::stdin())
-        }
-        _ => {
-            log::info!("Opening {input:?} ...");
-            Box::new(fs::File::open(input)?)
+    // backup_dir starts with the spool directory
+    let spool = &config.spool;
+    backup_dir.push(spool);
+
+    // next we add a vault
+    let vault_arg = matches.value_of("vault").unwrap_or("");
+    let vault_uuid = match uuid::Uuid::parse_str(vault_arg) {
+        Ok(uuid) => uuid.to_string(), // lower-case hyphenated UUID
+
+        Err(err) => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("Invalid vault {vault_arg} given: {err}"),
+            ));
         }
     };
 
-    let mut buffered_reader = io::BufReader::new(reader);
+    let vault_dir = build_canonical_path(&vault_uuid)?;
+    log::trace!("Using vault directory {vault_dir:?}");
+    backup_dir.push(vault_dir);
 
-    let state_home = config.base.get_state_home();
+    // then the output key, potentially containing a path of length >= 1
+    let output = matches.value_of("output").unwrap_or("");
 
-    let vault_dir = use_or_create_dir(&state_home, matches.value_of("vault").unwrap_or(""))?;
+    let output_dir = build_canonical_path(output)?;
+    log::trace!("Using output directory {output_dir:?}");
+    backup_dir.push(output_dir);
 
-    let output_dir = use_or_create_dir(&vault_dir, matches.value_of("output").unwrap_or(""))?;
+    // finally, the current UTC timestamp
+    let utc_now: DateTime<Utc> = Utc::now();
+    let utc_timestamp = utc_now.timestamp().to_string();
 
-    let splitter = Split::new(output_dir, 512);
+    let timestamp_dir = build_canonical_path(&utc_timestamp)?;
+    log::trace!("Using timestamp directory {timestamp_dir:?}");
+    backup_dir.push(timestamp_dir);
+
+    // mkdir -p backup_dir: let the first instance of two concurrent
+    // permafrust backup calls win in case they started with the same timestamp
+    // https://rcrowley.org/2010/01/06/things-unix-can-do-atomically.html
+    crate::use_dir_atomic_create_maybe(&backup_dir, Some(true), Some(true))?;
+
+    // TODO signal handling, Ctrl+C does not finish stream https://rust-cli.github.io/book/in-depth/signals.html
+    let splitter = Split::new(backup_dir, 512);
 
     let mut writer = match matches
         .value_of("compression")
@@ -52,12 +77,29 @@ pub fn perform_backup(config: Config, matches: &clap::ArgMatches) -> io::Result<
         _ => FinalEncoder::new(Box::new(splitter)),
     };
 
+    // setup input after we created the backup directory to prevent
+    // reading streams (or fifo files) that cannot be written later
+    let input = matches.value_of("input").unwrap_or("-");
+
+    let reader: Box<dyn io::Read> = match input {
+        "-" => {
+            log::info!("Reading from stdin ...");
+            Box::new(io::stdin())
+        }
+        _ => {
+            log::info!("Opening {input:?} ...");
+            Box::new(fs::File::open(input)?)
+        }
+    };
+
+    let mut buffered_reader = io::BufReader::new(reader);
+
     io::copy(&mut buffered_reader, &mut writer)?;
 
     Ok(())
 }
 
-fn use_or_create_dir(base: &Path, dir: &str) -> io::Result<PathBuf> {
+fn build_canonical_path(dir: &str) -> io::Result<PathBuf> {
     if dir.is_empty() {
         return Err(io::Error::new(
             io::ErrorKind::InvalidInput,
@@ -66,37 +108,25 @@ fn use_or_create_dir(base: &Path, dir: &str) -> io::Result<PathBuf> {
     }
 
     let dir_path = Path::new(dir);
-    let components: Vec<Component> = dir_path.components().collect();
+    let mut canonical_dir_path = PathBuf::new();
 
-    if components.len() != 1 {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("Invalid path {dir:?} given"),
-        ));
+    // create canonical representation
+    for component in dir_path.components() {
+        match component {
+            Component::Normal(subpath) => {
+                canonical_dir_path.push(subpath);
+            }
+            Component::CurDir => {
+                // ignore
+            }
+            _ => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    format!("Invalid path {dir:?} given"),
+                ));
+            }
+        }
     }
 
-    let base_dir_path = match components.first() {
-        Some(Component::Normal(_)) => base.join(dir_path),
-        _ => {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                format!("Invalid path {dir:?} given"),
-            ));
-        }
-    };
-
-    if let Err(err) = fs::read_dir(&base_dir_path) {
-        if err.kind() == io::ErrorKind::NotFound {
-            log::info!("Creating directory {base_dir_path:?}");
-            fs::create_dir(&base_dir_path)?;
-        } else {
-            // PermissionDenied or NotADirectory
-            log::error!("Cannot use path {base_dir_path:?}");
-            return Err(err);
-        }
-    } else {
-        log::trace!("Using directory {base_dir_path:?}");
-    }
-
-    Ok(base_dir_path)
+    Ok(canonical_dir_path)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@ use std::env;
 use std::fmt;
 use std::fs;
 use std::io;
-use std::path::PathBuf;
+use std::os::unix::fs::DirBuilderExt;
+use std::path::{Path, PathBuf};
 
 pub struct Config {
     pub base: xdg::BaseDirectories,
@@ -46,6 +47,46 @@ impl From<log::SetLoggerError> for CliError {
     fn from(error: log::SetLoggerError) -> Self {
         CliError::LogError(error, 1)
     }
+}
+
+fn use_dir_atomic_create_maybe(
+    dir_path: &Path,
+    create_dir: Option<bool>,
+    recursive: Option<bool>,
+) -> io::Result<()> {
+    if create_dir.unwrap_or(false) {
+        log::info!("Creating directory {dir_path:?}");
+        // first mkdir the parent path, ignoring if it exists, and then perfrom
+        // atomic creation of the final element in dir_path
+        // https://rcrowley.org/2010/01/06/things-unix-can-do-atomically.html
+        let mut builder = fs::DirBuilder::new();
+        builder.mode(0o755);
+
+        builder.recursive(recursive.unwrap_or(false));
+        if let Some(parent) = dir_path.parent() {
+            builder.create(parent).map_err(|err| {
+                io::Error::new(
+                    err.kind(),
+                    format!("Cannot create {path:?}: {err}", path = parent.display()),
+                )
+            })?;
+        }
+
+        // force failure if full dir_path already exists
+        builder.recursive(false);
+        builder.create(dir_path).map_err(|err| {
+            io::Error::new(
+                err.kind(),
+                format!("Cannot create {path:?}: {err}", path = dir_path.display()),
+            )
+        })?;
+    } else if let Err(err) = fs::read_dir(dir_path) {
+        // PermissionDenied, NotADirectory, NotFound, etc.
+        log::error!("Cannot use directory {dir_path:?}");
+        return Err(err);
+    }
+
+    Ok(())
 }
 
 fn use_base_dir(base: &xdg::BaseDirectories) -> io::Result<PathBuf> {
@@ -87,6 +128,10 @@ pub fn run(config: Config, command: &str, matches: &'_ clap::ArgMatches) -> Resu
     // setup base directory
     let base_pathbuf: PathBuf = use_base_dir(&config.base)?;
     log::trace!("Using base state directory {base_pathbuf:?}");
+
+    let spool = &config.spool;
+    use_dir_atomic_create_maybe(spool, None, None)?;
+    log::trace!("Using spool directory {spool:?}");
 
     // perform requested command
     match command {
