3729717186840864da7d285f2be23cd0b25319cd
Thomas Krennwallner <tk@postsubmeta.net>
2022-06-12 18:19:59 -0400

    lib: do not use profiles for xdg base directories

--- src/lib.rs
+++ src/lib.rs
@@ -138,8 +138,8 @@ fn use_base_dir(base: &xdg::BaseDirectories) -> io::Result<PathBuf> {
     }
 }
 
-pub fn base_directory_profile(subcommand: &str) -> Result<xdg::BaseDirectories, CliError> {
-    match xdg::BaseDirectories::with_profile(clap::crate_name!(), subcommand) {
+pub fn base_directory_profile(_subcommand: &str) -> Result<xdg::BaseDirectories, CliError> {
+    match xdg::BaseDirectories::with_prefix(clap::crate_name!()) {
         Ok(base_dirs) => Ok(base_dirs),
         Err(err) => Err(CliError::BaseDirError(err, CliResult::ConfigError)),
     }
