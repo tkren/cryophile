a111a0c21dd404dc73e2a1d2aa55963f3a742c3d
Thomas Krennwallner <tk@postsubmeta.net>
2022-06-12 18:21:20 -0400

    aws: add helper functions for aws-sdk

---
+++ src/aws.rs
@@ -0,0 +1,21 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3::{Client, Region};
+use aws_types::SdkConfig;
+use log::log_enabled;
+
+pub async fn aws_config(region: Option<String>) -> SdkConfig {
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("ca-central-1"));
+
+    if log_enabled!(log::Level::Trace) {
+        let region = region_provider.region().await.unwrap();
+        log::trace!("Using S3 region {region}")
+    }
+
+    aws_config::from_env().region(region_provider).load().await
+}
+
+async fn aws_client(config: &SdkConfig) -> Client {
+    Client::new(config)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod aws;
 mod backup;
 pub mod cli;
 pub mod constants;
