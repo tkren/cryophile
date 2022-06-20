1f86aa5d51f87c4c5faa09544e723cb306d4e0fe
Thomas Krennwallner <tk@postsubmeta.net>
2022-06-19 20:26:58 -0400

    freeze: read toml config file

--- src/freeze.rs
+++ src/freeze.rs
@@ -1,5 +1,6 @@
 use crate::aws;
 use crate::cli::{Cli, Freeze};
+use crate::configfile::ConfigFile;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use std::io;
 use std::sync::mpsc;
@@ -32,24 +33,30 @@ pub fn perform_freeze(
         }
     };
 
-    let config = if let Some(config) = &freeze.config {
+    let config_path = if let Some(config) = &freeze.config {
         config.to_path_buf()
     } else {
         base_directories.get_config_file("permafrust.toml")
     };
 
-    if config.exists() {
-        if let Err(err) = watcher.watch(&config, RecursiveMode::NonRecursive) {
-            log::error!("Cannot setup config watcher {config:?} failed: {err:?}");
-            return Err(io::Error::new(io::ErrorKind::Other, err.to_string()));
-        }
-        log::trace!("Watching config {path}", path = config.display());
+    let config = if config_path.exists() {
+        let config_file = match ConfigFile::new(&config_path) {
+            Err(err) => {
+                log::error!("Cannot parse config file {config_path:?}: {err}");
+                return Err(io::Error::new(io::ErrorKind::Other, err));
+            }
+            Ok(config_file) => config_file,
+        };
+        Some(config_file)
     } else {
         log::warn!(
             "Configuration {path} does not exist, ignoring configuration updates",
-            path = config.display()
+            path = config_path.display()
         );
-    }
+        None
+    };
+
+    log::trace!("Config: {config:#?}");
 
     let spool = cli.spool.as_ref();
     if let Err(err) = watcher.watch(spool, RecursiveMode::Recursive) {
@@ -59,7 +66,7 @@ pub fn perform_freeze(
     log::trace!("Watching spool {path}", path = spool.display());
 
     for res in rx {
-        if let Err(err) = event_handler(res) {
+        if let Err(err) = event_handler(res, &config) {
             return Err(io::Error::new(io::ErrorKind::Other, err.to_string()));
         }
     }
@@ -67,7 +74,10 @@ pub fn perform_freeze(
     Ok(())
 }
 
-fn event_handler(result: Result<notify::Event, notify::Error>) -> Result<(), notify::Error> {
+fn event_handler(
+    result: Result<notify::Event, notify::Error>,
+    config: &Option<ConfigFile>,
+) -> Result<(), notify::Error> {
     match result {
         Ok(notify::Event { kind, paths, attrs }) => {
             log::debug!("Event: {kind:?} {paths:?} {attrs:?}")
