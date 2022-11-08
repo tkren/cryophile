e5d9565cfb5a33752ab116eb8970e48eefcfb900
Thomas Krennwallner <tk@postsubmeta.net>
2022-11-08 14:28:58 -0500

    command: use thread_io for compression thread

--- src/cli/constants.rs
+++ src/cli/constants.rs
@@ -1,3 +1,5 @@
-pub static DEFAULT_CHUNK_SIZE: usize = 512;
+pub const DEFAULT_BUF_SIZE: usize = 8192;
+
+pub const DEFAULT_CHUNK_SIZE: usize = 512;
 
 pub static DEFAULT_SPOOL_PATH: &str = "/var/spool/permafrust";
--- src/command/backup.rs
+++ src/command/backup.rs
@@ -1,25 +1,116 @@
+use crate::cli::constants::DEFAULT_BUF_SIZE;
 use crate::cli::Backup;
 use crate::cli::Cli;
 use crate::compression::CompressionType;
 use crate::core::Split;
 use crate::crypto::openpgp::openpgp_error;
 use crate::crypto::openpgp::Keyring;
-use age::Recipient;
 
+use sequoia_openpgp::policy::Policy;
 use sequoia_openpgp::policy::StandardPolicy;
 use sequoia_openpgp::serialize::stream::Encryptor;
 use sequoia_openpgp::serialize::stream::LiteralWriter;
 use sequoia_openpgp::serialize::stream::Message;
 use sequoia_openpgp::types::DataFormat;
 use sequoia_openpgp::types::SymmetricAlgorithm;
+use sequoia_openpgp::Cert;
 
 use std::fs;
 use std::io;
+use std::io::Write;
 use std::path::{Component, Path, PathBuf};
 
 pub fn perform_backup(cli: &Cli, backup: &Backup) -> io::Result<()> {
     log::info!("BACKUP...");
 
+    let backup_dir = build_backup_path(cli, backup)?;
+
+    let mut recipients: Vec<Box<dyn age::Recipient>> = vec![];
+    if backup.recipient.is_some() {
+        for recipient in backup.recipient.as_ref().unwrap() {
+            recipients.push(recipient.get_recipient());
+        }
+    }
+
+    /*
+    log::debug!(
+        "Age Recipients: {recipients:?}",
+        recipients = backup.recipient
+    );
+    log::debug!("OpenPGP Keyring: {keyring:?}", keyring = backup.keyring);
+    */
+    if backup.keyring.is_empty() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Keyring is empty",
+        ));
+    }
+
+    // get certificates from keyring
+    let policy = StandardPolicy::new();
+    let cert_list: Keyring = parse_keyring(&policy, backup.keyring.iter().flatten())?;
+
+    // setup backup directory and splitter encryption sink
+    // after we have some certificates for storage encryption
+
+    // mkdir -p backup_dir: let the first instance of two concurrent
+    // permafrust backup calls win in case they started with the same timestamp
+    // https://rcrowley.org/2010/01/06/things-unix-can-do-atomically.html
+    crate::use_dir_atomic_create_maybe(&backup_dir, Some(true), Some(true))?;
+
+    // TODO signal handling, Ctrl+C does not finish stream https://rust-cli.github.io/book/in-depth/signals.html
+    let mut splitter = Split::new(backup_dir, backup.size);
+
+    let mut encryptor_sink = build_encryptor(cert_list, &mut splitter)?;
+
+    // setup input after we created the backup directory and setup encryption to prevent
+    // reading streams (or fifo files) that cannot be written later
+    let reader: Box<dyn io::Read> = build_reader(backup.input.as_ref())?;
+    let mut buffered_reader = io::BufReader::new(reader);
+
+    log::trace!("Starting backup ...");
+
+    let copy_result = match backup.compression {
+        CompressionType::None => io::copy(&mut buffered_reader, &mut encryptor_sink)?,
+        CompressionType::Zstd => thread_io::write::writer(
+            DEFAULT_BUF_SIZE,
+            1,
+            &mut encryptor_sink,
+            |writer| -> io::Result<u64> {
+                let mut zstd_encoder = zstd::stream::Encoder::new(writer, 0)?;
+                let result = compressor_worker(&mut buffered_reader, &mut zstd_encoder);
+                if result.is_ok() {
+                    zstd_encoder.do_finish()?
+                }
+                result
+            },
+        )?,
+        CompressionType::Lz4 => thread_io::write::writer(
+            DEFAULT_BUF_SIZE,
+            1,
+            &mut encryptor_sink,
+            |writer| -> io::Result<u64> {
+                let mut lz4_encoder = lz4_flex::frame::FrameEncoder::new(writer);
+                let result = compressor_worker(&mut buffered_reader, &mut lz4_encoder);
+                if result.is_ok() {
+                    lz4_encoder.try_finish()?
+                }
+                result
+            },
+        )?,
+    };
+
+    log::trace!("Wrote total of {copy_result} bytes");
+    encryptor_sink.flush()?;
+    encryptor_sink.finalize().map_err(openpgp_error)
+}
+
+fn compressor_worker(reader: &mut dyn io::Read, compressor: &mut dyn io::Write) -> io::Result<u64> {
+    log::trace!("Starting compressor worker ...");
+    io::copy(reader, compressor)
+}
+
+fn build_backup_path(cli: &Cli, backup: &Backup) -> io::Result<PathBuf> {
     let mut backup_dir = PathBuf::new();
 
     // backup_dir starts with the spool directory
@@ -52,34 +143,19 @@ pub fn perform_backup(cli: &Cli, backup: &Backup) -> io::Result<()> {
     log::trace!("Using timestamp directory {timestamp_dir:?}");
     backup_dir.push(timestamp_dir);
 
-    let mut recipients: Vec<Box<dyn Recipient>> = vec![];
-    if backup.recipient.is_some() {
-        for recipient in backup.recipient.as_ref().unwrap() {
-            recipients.push(recipient.get_recipient());
-        }
-    }
+    Ok(backup_dir)
+}
 
-    /*
-    log::debug!(
-        "Age Recipients: {recipients:?}",
-        recipients = backup.recipient
-    );
-    log::debug!("OpenPGP Keyring: {keyring:?}", keyring = backup.keyring);
-    */
-    if backup.keyring.is_empty() {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Keyring is empty",
-        ));
-    }
-
+fn parse_keyring<'a, K>(policy: &'a dyn Policy, keyring: K) -> io::Result<Keyring<'a>>
+where
+    K: Iterator<Item = &'a Cert>,
+{
     // get certificates from keyring
-    let policy = StandardPolicy::new();
     let mut cert_list: Keyring = Vec::new();
-    for cert in backup.keyring.iter().flatten() {
+    for cert in keyring {
         for storage in cert
             .keys()
-            .with_policy(&policy, None)
+            .with_policy(policy, None)
             .supported()
             .alive()
             .revoked(false)
@@ -108,21 +184,21 @@ pub fn perform_backup(cli: &Cli, backup: &Backup) -> io::Result<()> {
         ));
     }
 
-    // setup backup directory and splitter encryption sink
-    // after we have some certificates for storage encryption
+    Ok(cert_list)
+}
 
-    // mkdir -p backup_dir: let the first instance of two concurrent
-    // permafrust backup calls win in case they started with the same timestamp
-    // https://rcrowley.org/2010/01/06/things-unix-can-do-atomically.html
-    crate::use_dir_atomic_create_maybe(&backup_dir, Some(true), Some(true))?;
-
-    // TODO signal handling, Ctrl+C does not finish stream https://rust-cli.github.io/book/in-depth/signals.html
-    let splitter = Split::new(backup_dir, backup.size);
-
+fn build_encryptor<'a, R, W: 'a + io::Write + Send + Sync>(
+    recipients: R,
+    output: W,
+) -> io::Result<Message<'a>>
+where
+    R: IntoIterator,
+    R::Item: Into<sequoia_openpgp::serialize::stream::Recipient<'a>>,
+{
     log::trace!("Setting up encryption ...");
-    let message = Message::new(splitter);
+    let message = Message::new(output);
     let encryptor =
-        Encryptor::for_recipients(message, cert_list).symmetric_algo(SymmetricAlgorithm::AES256);
+        Encryptor::for_recipients(message, recipients).symmetric_algo(SymmetricAlgorithm::AES256);
 
     // Encrypt the message.
     log::trace!("Starting encryption ...");
@@ -130,34 +206,10 @@ pub fn perform_backup(cli: &Cli, backup: &Backup) -> io::Result<()> {
 
     // Literal wrapping.
     log::trace!("Setting up encryption stream ...");
-    let mut message = LiteralWriter::new(message)
+    LiteralWriter::new(message)
         .format(DataFormat::Binary)
         .build()
-        .map_err(openpgp_error)?;
-
-    /*
-    let mut writer = match backup.compression {
-        CompressionType::Zstd => {
-            let zstd_encoder = zstd::stream::Encoder::new(encryptor, 0)?;
-            FinalEncoder::new(Box::new(zstd_encoder))
-        }
-        CompressionType::Lz4 => {
-            let lz4_encoder = lz4_flex::frame::FrameEncoder::new(encryptor);
-            FinalEncoder::new(Box::new(lz4_encoder))
-        }
-        CompressionType::None => FinalEncoder::new(Box::new(encryptor)),
-    }; */
-
-    // setup input after we created the backup directory and setup encryption to prevent
-    // reading streams (or fifo files) that cannot be written later
-    let reader: Box<dyn io::Read> = build_reader(backup.input.as_ref())?;
-    let mut buffered_reader = io::BufReader::new(reader);
-
-    log::trace!("Starting backup ...");
-    let copy_result = io::copy(&mut buffered_reader, &mut message)?;
-    log::trace!("Wrote {copy_result} bytes");
-
-    message.finalize().map_err(openpgp_error)
+        .map_err(openpgp_error)
 }
 
 fn build_reader(path: Option<&PathBuf>) -> io::Result<Box<dyn io::Read>> {
