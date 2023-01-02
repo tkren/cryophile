c3c3aa6ee1c05f519f0fb4fa9ba984ad47793eb9
Thomas Krennwallner <tk@postsubmeta.net>
2023-01-02 09:24:16 -0500

    cli: move subcommands and parse helper functions out of mod.rs

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,20 +1,16 @@
 pub mod constants;
 pub mod error;
+pub mod parse;
 pub mod result;
+mod subcommand;
 
-use crate::compression::CompressionType;
-use crate::crypto::age::RecipientSpec;
-use crate::crypto::openpgp::openpgp_error;
-use clap::{value_parser, Parser, Subcommand};
-use sequoia_openpgp::cert::CertParser;
-use sequoia_openpgp::parse::Parse;
-use sequoia_openpgp::Cert;
-use std::fmt;
+use clap::{value_parser, Parser};
 use std::path::PathBuf;
 
 pub use self::constants::{DEFAULT_CHUNK_SIZE, DEFAULT_SPOOL_PATH};
 pub use self::error::CliError;
 pub use self::result::CliResult;
+pub use self::subcommand::{Backup, Command, Freeze, Restore, Thaw};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = clap::crate_description!())]
@@ -46,118 +42,4 @@ pub struct Cli {
     /// Quiet mode
     #[arg(short, long, help = "Quiet mode")]
     pub quiet: bool,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Command {
-    /// Queue input for upload
-    #[command(arg_required_else_help = false)]
-    Backup(Backup),
-    /// Upload backup
-    #[command(arg_required_else_help = false)]
-    Freeze(Freeze),
-    /// Download backup
-    #[command(arg_required_else_help = false)]
-    Thaw(Thaw),
-    /// Uncompress and decrypt downloaded backup files
-    #[command(arg_required_else_help = false)]
-    Restore(Restore),
-}
-
-impl fmt::Display for Command {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let command_name = match self {
-            Command::Backup(_) => "backup",
-            Command::Freeze(_) => "freeze",
-            Command::Thaw(_) => "thaw",
-            Command::Restore(_) => "restore",
-        };
-        write!(f, "{command_name}")
-    }
-}
-
-#[derive(Parser, Debug)]
-#[command(about = "Not shown")]
-pub struct Backup {
-    #[arg(short = 'C', long, help = "compression type", value_enum, default_value_t = CompressionType::default())]
-    pub compression: CompressionType,
-
-    #[arg(short, long, help = "input file", value_parser = value_parser!(PathBuf))]
-    pub input: Option<PathBuf>,
-
-    #[arg(short, long, help = "keyring", action = clap::ArgAction::Append, required = true, value_parser = parse_keyring)]
-    pub keyring: Vec<Vec<Cert>>,
-
-    #[arg(short, long, help = "output path in vault", value_parser = value_parser!(PathBuf))]
-    pub output: Option<PathBuf>,
-
-    #[arg(short, long, help = "recipient", value_parser = parse_recipient)]
-    pub recipient: Option<Vec<RecipientSpec>>,
-
-    #[arg(short, long, help = "chunk size", value_parser = parse_chunk_size, default_value_t = DEFAULT_CHUNK_SIZE)]
-    pub size: usize,
-
-    #[arg(short, long, help = "vault", value_parser = parse_uuid)]
-    pub vault: uuid::Uuid,
-}
-
-#[derive(Parser, Debug)]
-#[command(about = "Not shown")]
-pub struct Freeze {
-    #[arg(short, long, help = "config file", value_parser = value_parser!(PathBuf))]
-    pub config: Option<PathBuf>,
-}
-
-#[derive(Parser, Debug)]
-#[command(about = "Not shown")]
-pub struct Thaw {}
-
-#[derive(Parser, Debug)]
-#[command(about = "Not shown")]
-pub struct Restore {
-    #[arg(short, long, help = "output file", value_parser = value_parser!(PathBuf))]
-    pub output: Option<PathBuf>,
-
-    #[arg(short, long, help = "vault", value_parser = parse_uuid)]
-    pub vault: uuid::Uuid,
-}
-
-fn parse_chunk_size(s: &str) -> Result<usize, String> {
-    let parse_config = parse_size::Config::new()
-        .with_binary()
-        .with_byte_suffix(parse_size::ByteSuffix::Deny);
-    let parse_size_result = parse_config
-        .parse_size(s)
-        .map_err(|e| format!("Cannot parse chunk size: {e}"))?;
-    let chunk_size = usize::try_from(parse_size_result)
-        .map_err(|e| format!("Cannot parse chunk size (size exceeds usize): {e}"))?;
-    Ok(chunk_size)
-}
-
-fn parse_uuid(s: &str) -> Result<uuid::Uuid, String> {
-    let uuid = uuid::Uuid::parse_str(s).map_err(|e| format!("Cannot parse uuid: {e}"))?;
-    Ok(uuid)
-}
-
-fn parse_recipient(s: &str) -> Result<RecipientSpec, String> {
-    let recipient = s
-        .parse::<RecipientSpec>()
-        .map_err(|e| format!("Cannot parse age: {e}"))?;
-    Ok(recipient)
-}
-
-fn parse_keyring(s: &str) -> Result<Vec<Cert>, String> {
-    let mut cert_list: Vec<Cert> = Vec::new();
-    let parser = CertParser::from_file(s).map_err(|e| openpgp_error(e).to_string())?;
-    for parsed_cert in parser {
-        if let Err(err) = parsed_cert {
-            return Err(openpgp_error(err).to_string());
-        }
-        let result: Cert = parsed_cert.unwrap();
-        cert_list.push(result);
-    }
-    if cert_list.is_empty() {
-        return Err(format!("Keyring {s} is empty"));
-    }
-    Ok(cert_list)
 }
---
+++ src/cli/parse.rs
@@ -0,0 +1,45 @@
+use crate::crypto::age::RecipientSpec;
+use crate::crypto::openpgp::openpgp_error;
+use sequoia_openpgp::cert::CertParser;
+use sequoia_openpgp::parse::Parse;
+use sequoia_openpgp::Cert;
+
+pub(crate) fn parse_chunk_size(s: &str) -> Result<usize, String> {
+    let parse_config = parse_size::Config::new()
+        .with_binary()
+        .with_byte_suffix(parse_size::ByteSuffix::Deny);
+    let parse_size_result = parse_config
+        .parse_size(s)
+        .map_err(|e| format!("Cannot parse chunk size: {e}"))?;
+    let chunk_size = usize::try_from(parse_size_result)
+        .map_err(|e| format!("Cannot parse chunk size (size exceeds usize): {e}"))?;
+    Ok(chunk_size)
+}
+
+pub(crate) fn parse_uuid(s: &str) -> Result<uuid::Uuid, String> {
+    let uuid = uuid::Uuid::parse_str(s).map_err(|e| format!("Cannot parse uuid: {e}"))?;
+    Ok(uuid)
+}
+
+pub(crate) fn parse_recipient(s: &str) -> Result<RecipientSpec, String> {
+    let recipient = s
+        .parse::<RecipientSpec>()
+        .map_err(|e| format!("Cannot parse age: {e}"))?;
+    Ok(recipient)
+}
+
+pub(crate) fn parse_keyring(s: &str) -> Result<Vec<Cert>, String> {
+    let mut cert_list: Vec<Cert> = Vec::new();
+    let parser = CertParser::from_file(s).map_err(|e| openpgp_error(e).to_string())?;
+    for parsed_cert in parser {
+        if let Err(err) = parsed_cert {
+            return Err(openpgp_error(err).to_string());
+        }
+        let result: Cert = parsed_cert.unwrap();
+        cert_list.push(result);
+    }
+    if cert_list.is_empty() {
+        return Err(format!("Keyring {s} is empty"));
+    }
+    Ok(cert_list)
+}
