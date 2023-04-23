9ddf91af2c19e8b58449d704e4ef2d4a82bd67d2
Thomas Krennwallner <tk@postsubmeta.net>
2023-04-23 18:11:10 -0400

    command: fix freeze event loop

--- src/command/freeze.rs
+++ src/command/freeze.rs
@@ -2,9 +2,11 @@ use crate::cli::{Cli, Freeze};
 use crate::config::ConfigFile;
 use crate::core::aws;
 use crate::core::notify::notify_error;
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
-use std::io;
+use notify::event::{AccessKind, AccessMode};
+use notify::{EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
 use std::sync::mpsc;
+use std::{fs, io};
 use xdg::BaseDirectories;
 
 pub fn perform_freeze(
@@ -46,37 +48,75 @@ pub fn perform_freeze(
         };
         Some(config_file)
     } else {
-        log::warn!(
-            "Configuration {path} does not exist, ignoring configuration updates",
-            path = config_path.display()
-        );
+        log::warn!("Configuration {config_path:?} does not exist, ignoring configuration updates");
         None
     };
 
     log::trace!("Config: {config:#?}");
 
     let spool = cli.spool.as_ref();
-    watcher
-        .watch(spool, RecursiveMode::Recursive)
-        .map_err(notify_error)?;
-    log::trace!("Watching spool {path}", path = spool.display());
+    watch_read_dir(&mut watcher, spool)?;
+    log::debug!("Watching spool {spool:?}");
 
     for res in rx {
-        event_handler(res).map_err(notify_error)?
+        event_handler(res, &mut watcher).map_err(notify_error)?;
     }
 
     Ok(())
 }
 
-fn event_handler(result: Result<notify::Event, notify::Error>) -> Result<(), notify::Error> {
+fn watch_read_dir(watcher: &mut notify::RecommendedWatcher, path: &Path) -> io::Result<()> {
+    if !path.is_dir() {
+        log::warn!("Ignoring non-directory: {path:?}");
+        return Ok(());
+    }
+
+    watcher
+        .watch(path, RecursiveMode::NonRecursive)
+        .map_err(notify_error)?;
+    log::debug!("Watching path {path:?}");
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let sub_path = entry.path();
+        if sub_path.is_dir() {
+            // TODO only watch configured vaults
+            watcher
+                .watch(sub_path.as_path(), RecursiveMode::NonRecursive)
+                .map_err(notify_error)?;
+            log::trace!("Watching subdirectory: {sub_path:?}")
+            // TODO read_dir vault
+        } else {
+            log::warn!("Ignoring non-directory: {sub_path:?}")
+        }
+    }
+
+    Ok(())
+}
+
+fn event_handler(
+    result: Result<notify::Event, notify::Error>,
+    watcher: &mut notify::RecommendedWatcher,
+) -> Result<(), notify::Error> {
+    // TODO check which level in spool causes the event
+    // TODO inside vault: new backup dirs arrive, add them if they are not yet uploaded, if uploaded unwatch backup_dir
+    // TODO if chunk.0 file arrives, backup is done, do another read_dir for files that are not in "w" mode
     match result {
+        Ok(notify::Event { kind, paths, attrs })
+            if kind == EventKind::Access(AccessKind::Close(AccessMode::Write)) =>
+        {
+            log::info!("Closed file event: {kind:?} {paths:?} {attrs:?}");
+            for path in paths {
+                watch_read_dir(watcher, &path)?;
+            }
+        }
         Ok(notify::Event { kind, paths, attrs }) => {
-            log::debug!("Event: {kind:?} {paths:?} {attrs:?}")
+            log::debug!("Event: {kind:?} {paths:?} {attrs:?}");
         }
         Err(err) => {
             log::error!("watch error: {err:?}");
             return Err(err);
         }
-    }
+    };
     Ok(())
 }
