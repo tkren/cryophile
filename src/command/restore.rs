9d3930c38c62dbd5d31557cce7ac500955706b5c
Thomas Krennwallner <tk@postsubmeta.net>
2023-08-08 13:36:25 -0400

    command: basic restore is implemented

--- src/command/restore.rs
+++ src/command/restore.rs
@@ -7,13 +7,12 @@ use notify::event::{AccessKind, AccessMode, CreateKind};
 use notify::{EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
+use std::os::unix::prelude::OpenOptionsExt;
 use std::path::{Path, PathBuf};
 use std::{fs, io, thread};
 
-pub fn perform_restore(cli: &Cli, restore: &Restore) -> io::Result<()> {
-    log::info!("RESTORE…");
-
-    let _output: Box<dyn io::Write> = match &restore.output {
+fn build_writer(path: Option<&PathBuf>) -> io::Result<Box<dyn io::Write>> {
+    let writer: Box<dyn io::Write> = match path {
         Some(p) if p.as_path() == Path::new("-") => {
             log::info!("Writing to stdout…");
             Box::new(io::stdout())
@@ -23,13 +22,26 @@ pub fn perform_restore(cli: &Cli, restore: &Restore) -> io::Result<()> {
             Box::new(io::stdout())
         }
         Some(output) => {
-            log::info!("Opening {output:?}…");
-            Box::new(fs::File::open(output)?)
+            log::info!("Creating restore output {output:?}");
+            Box::new(
+                fs::OpenOptions::new()
+                    .write(true)
+                    .create_new(true)
+                    .mode(0o600)
+                    .open(output)?,
+            )
         }
     };
+    Ok(writer)
+}
+
+pub fn perform_restore(cli: &Cli, restore: &Restore) -> io::Result<()> {
+    log::info!("RESTORE…");
+
+    let output: Box<dyn io::Write> = build_writer(restore.output.as_ref())?;
 
     let backup_path_components: BackupPathComponents =
-        (cli.spool.clone(), restore.vault, restore.output.clone()).into();
+        (cli.spool.clone(), restore.vault, restore.prefix.clone()).into();
 
     let backup_dir: Option<PathBuf> = (&backup_path_components).into();
     let Some(backup_dir) = backup_dir else {
@@ -48,9 +60,12 @@ pub fn perform_restore(cli: &Cli, restore: &Restore) -> io::Result<()> {
 
     let (event_sender, fragment_receiver) = crossbeam::channel::unbounded::<Option<Fragment>>();
 
-    thread::spawn(move || fragment_worker(fragment_receiver));
+    thread::spawn(move || {
+        notify_event_worker(&backup_dir, &mut watcher, &notify_receiver, &event_sender)
+    });
 
-    notify_event_worker(&backup_dir, &mut watcher, &notify_receiver, &event_sender)?;
+    let copy_result = fragment_worker(fragment_receiver, output)?;
+    log::info!("Received total of {copy_result} bytes");
 
     Ok(())
 }
@@ -63,18 +78,26 @@ fn channel_recv_error(e: crossbeam::channel::RecvError) -> io::Error {
     io::Error::new(io::ErrorKind::Other, format!("Channel recv error: {e}"))
 }
 
-fn fragment_worker(receiver: crossbeam::channel::Receiver<Option<Fragment>>) -> io::Result<()> {
+fn fragment_worker(
+    receiver: crossbeam::channel::Receiver<Option<Fragment>>,
+    output: Box<dyn io::Write>,
+) -> io::Result<u64> {
     log::trace!("Starting fragment_worker…");
+    let mut bytes_written: u64 = 0;
+    let mut buffered_writer = io::BufWriter::new(output);
     loop {
         match receiver.recv().map_err(channel_recv_error)? {
             None => break,
             Some(item) => {
                 log::trace!("Received fragment {item}");
+                let fragment = fs::OpenOptions::new().read(true).open(item.path)?;
+                let mut reader = io::BufReader::new(fragment);
+                bytes_written += io::copy(&mut reader, &mut buffered_writer)?
             }
         }
     }
     log::trace!("Finishing fragment_worker…");
-    Ok(())
+    Ok(bytes_written)
 }
 
 fn send_or_push_fragment(
@@ -105,10 +128,11 @@ fn notify_event_worker(
     notify_receiver: &Receiver<Result<notify::Event, notify::Error>>,
     sender: &Sender<Option<Fragment>>,
 ) -> io::Result<()> {
-    log::trace!("Starting notify_event_worker");
+    log::trace!("Starting notify_event_worker…");
 
     let mut heap: BinaryHeap<Fragment> = BinaryHeap::new();
     let mut current_priority = Reverse(1);
+    let mut zero_received: bool = false;
 
     for result in notify_receiver {
         match &result.map_err(notify_error)? {
@@ -132,6 +156,11 @@ fn notify_event_worker(
                 log::debug!("Received restore fragment: {kind:?} {paths:?} {attrs:?}");
                 for path in paths {
                     let Some(current_fragment) = Fragment::new(path.as_path()) else {continue;};
+                    if current_fragment.priority == Reverse(0) {
+                        log::trace!("Received zero fragment: {current_fragment:?}");
+                        zero_received = true;
+                        continue;
+                    }
                     current_priority = send_or_push_fragment(
                         sender,
                         &mut heap,
@@ -140,8 +169,8 @@ fn notify_event_worker(
                     )?;
                 }
             }
-            notify::Event { kind, paths, attrs } => {
-                log::trace!("Ignoring event {kind:?} {paths:?} {attrs:?}");
+            notify::Event { .. } => {
+                //log::trace!("Ignoring event {kind:?} {paths:?} {attrs:?}");
                 continue;
             }
         }
@@ -156,6 +185,11 @@ fn notify_event_worker(
             } else {
                 current_priority = next_priority; // we found a fragment, let's search for next_priority
             }
+        }
+        if zero_received {
+            // we found the zero file, signal shutdown
+            sender.send(None).map_err(channel_send_error)?;
+            break;
         }
     }
     log::trace!("Finishing notify_event_worker…");
