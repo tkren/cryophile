a7a140f49639416bad372801f17af9c0581f986d
Thomas Krennwallner <tk@postsubmeta.net>
2023-08-07 22:23:31 -0400

    command: use walkdir for finding pre-materialized backup chunks

--- src/command/freeze.rs
+++ src/command/freeze.rs
@@ -2,11 +2,12 @@ use crate::cli::{Cli, Freeze};
 use crate::config::ConfigFile;
 use crate::core::aws;
 use crate::core::notify::notify_error;
-use notify::event::{AccessKind, AccessMode};
+use notify::event::{AccessKind, AccessMode, CreateKind, RemoveKind};
 use notify::{EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::Path;
 use std::sync::mpsc;
 use std::{fs, io};
+use walkdir::WalkDir;
 use xdg::BaseDirectories;
 
 pub fn perform_freeze(
@@ -55,36 +56,55 @@ pub fn perform_freeze(
     log::trace!("Config: {config:#?}");
 
     let spool = cli.spool.as_ref();
-    watch_read_dir(&mut watcher, spool)?;
+    watch_read_dir(&mut watcher, spool, RecursiveMode::Recursive)?;
     log::debug!("Watching spool {spool:?}");
 
     for res in rx {
-        event_handler(res, &mut watcher).map_err(notify_error)?;
+        event_handler(res, spool, &mut watcher).map_err(notify_error)?;
     }
 
     Ok(())
 }
 
-fn watch_read_dir(watcher: &mut notify::RecommendedWatcher, path: &Path) -> io::Result<()> {
+fn watch_read_dir(
+    watcher: &mut notify::RecommendedWatcher,
+    path: &Path,
+    mode: RecursiveMode,
+) -> io::Result<()> {
     if !path.is_dir() {
         log::warn!("Ignoring non-directory: {path:?}");
         return Ok(());
     }
 
-    watcher
-        .watch(path, RecursiveMode::NonRecursive)
-        .map_err(notify_error)?;
-    log::debug!("Watching path {path:?}");
+    watcher.watch(path, mode).map_err(notify_error)?;
+    log::debug!("Watching path ({mode:?}): {path:?}");
+
+    for entry in WalkDir::new(path) {
+        if let Err(e) = &entry {
+            log::warn!("Cannot walk {entry:?}, ignoring: {e}");
+            continue;
+        } else if let Ok(dir_entry) = &entry {
+            if !dir_entry.path_is_symlink() {
+                let dir_entry_path = dir_entry.path();
+                if dir_entry_path.is_file() {
+                    log::debug!("Found {dir_entry_path:?}");
+                    // TODO found file may or may not be open for writing
+                    continue;
+                }
+            }
+            log::debug!("Ignoring {dir_entry:?}");
+        }
+    }
 
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let sub_path = entry.path();
         if sub_path.is_dir() {
             // TODO only watch configured vaults
-            watcher
-                .watch(sub_path.as_path(), RecursiveMode::NonRecursive)
-                .map_err(notify_error)?;
-            log::trace!("Watching subdirectory: {sub_path:?}")
+            //watcher
+            //    .watch(sub_path.as_path(), mode)
+            //    .map_err(notify_error)?;
+            log::trace!("Not watching subdirectory ({mode:?}): {sub_path:?}")
             // TODO read_dir vault
         } else {
             log::warn!("Ignoring non-directory: {sub_path:?}")
@@ -96,18 +116,87 @@ fn watch_read_dir(watcher: &mut notify::RecommendedWatcher, path: &Path) -> io::
 
 fn event_handler(
     result: Result<notify::Event, notify::Error>,
-    watcher: &mut notify::RecommendedWatcher,
+    spool: &Path,
+    _watcher: &mut notify::RecommendedWatcher,
 ) -> Result<(), notify::Error> {
     // TODO check which level in spool causes the event
     // TODO inside vault: new backup dirs arrive, add them if they are not yet uploaded, if uploaded unwatch backup_dir
     // TODO if chunk.0 file arrives, backup is done, do another read_dir for files that are not in "w" mode
     match result {
+        Ok(notify::Event { kind, paths, attrs }) if kind == EventKind::Create(CreateKind::File) => {
+            log::info!("Create file event: {kind:?} {paths:?} {attrs:?}");
+            for path in &paths {
+                if let Some(parent) = path.parent() {
+                    if parent == spool {
+                        //watch_read_dir(watcher, &path, RecursiveMode::Recursive)?;
+                        log::debug!(
+                            "Ignoring spool create file event: {kind:?} {paths:?} {attrs:?}"
+                        );
+                    } else {
+                        log::debug!("Ignoring watched file event: {kind:?} {paths:?} {attrs:?}");
+                    }
+                } else {
+                    log::debug!("Ignoring create root file event: {kind:?} {paths:?} {attrs:?}");
+                }
+            }
+        }
         Ok(notify::Event { kind, paths, attrs })
             if kind == EventKind::Access(AccessKind::Close(AccessMode::Write)) =>
         {
-            log::info!("Closed file event: {kind:?} {paths:?} {attrs:?}");
-            for path in paths {
-                watch_read_dir(watcher, &path)?;
+            log::info!("Close file event: {kind:?} {paths:?} {attrs:?}");
+            for path in &paths {
+                if let Some(parent) = path.parent() {
+                    if parent == spool {
+                        //watch_read_dir(watcher, &path, RecursiveMode::Recursive)?;
+                        log::debug!(
+                            "Ignoring spool close file event: {kind:?} {paths:?} {attrs:?}"
+                        );
+                    } else {
+                        log::debug!("Ignoring watched close event: {kind:?} {paths:?} {attrs:?}");
+                    }
+                } else {
+                    log::debug!("Ignoring create root close event: {kind:?} {paths:?} {attrs:?}");
+                }
+            }
+        }
+        Ok(notify::Event { kind, paths, attrs })
+            if kind == EventKind::Create(CreateKind::Folder) =>
+        {
+            log::info!("Create folder event: {kind:?} {paths:?} {attrs:?}");
+            for path in &paths {
+                if let Some(parent) = path.parent() {
+                    if parent == spool {
+                        //watch_read_dir(watcher, &path, RecursiveMode::Recursive)?;
+                        log::debug!(
+                            "Ignoring spool create folder event: {kind:?} {paths:?} {attrs:?}"
+                        );
+                    } else {
+                        log::debug!("Ignoring watched folder event: {kind:?} {paths:?} {attrs:?}");
+                    }
+                } else {
+                    log::debug!("Ignoring create root folder event: {kind:?} {paths:?} {attrs:?}");
+                }
+            }
+        }
+        Ok(notify::Event { kind, paths, attrs })
+            if kind == EventKind::Remove(RemoveKind::Folder) =>
+        {
+            log::info!("Remove folder event: {kind:?} {paths:?} {attrs:?}");
+            for path in &paths {
+                if let Some(parent) = path.parent() {
+                    if parent == spool {
+                        //watch_read_dir(watcher, &path, RecursiveMode::Recursive)?;
+                        log::debug!(
+                            "Ignoring spool remove folder event: {kind:?} {paths:?} {attrs:?}"
+                        );
+                    } else {
+                        log::debug!(
+                            "Ignoring watched remove folder event: {kind:?} {paths:?} {attrs:?}"
+                        );
+                    }
+                } else {
+                    log::debug!("Ignoring remove root folder event: {kind:?} {paths:?} {attrs:?}");
+                }
             }
         }
         Ok(notify::Event { kind, paths, attrs }) => {
