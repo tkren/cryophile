9eb12710b3a4e91185caa31acb17be91d1315c4e
Thomas Krennwallner <tk@postsubmeta.net>
2023-08-10 07:14:28 -0400

    COPYRIGHT: license under Apache-2.0 or MIT
    - Add COPYRIGHT file
    - Add LICENSE-APACHE and LICENSE-MIT for copyright text
    - Add copyright notice to source code

--- src/command/backup.rs
+++ src/command/backup.rs
@@ -1,3 +1,12 @@
+// Copyright The Permafrust Authors.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE> or
+// <http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT> or <http://opensource.org/licenses/MIT>, at your option.
+//
+// This file may not be copied, modified, or distributed except according
+// to those terms.
+
 use crate::cli::{constants::DEFAULT_BUF_SIZE, Backup, Cli};
 use crate::compression::CompressionType;
 use crate::core::constants::{CHUNK_FILE_MODE, CHUNK_FILE_PREFIX};
--- src/core/aws.rs
+++ src/core/aws.rs
@@ -1,3 +1,12 @@
+// Copyright The Permafrust Authors.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE> or
+// <http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT> or <http://opensource.org/licenses/MIT>, at your option.
+//
+// This file may not be copied, modified, or distributed except according
+// to those terms.
+
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{config::Region, Client};
 use aws_types::SdkConfig;
