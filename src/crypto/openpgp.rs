50d39ca248a6fe099dba5567335d4a80b795f4cb
Thomas Krennwallner <tk@postsubmeta.net>
2023-08-12 05:54:26 -0400

    crypto: add build_decryptor for openpgp

--- src/crypto/openpgp.rs
+++ src/crypto/openpgp.rs
@@ -7,12 +7,23 @@
 // This file may not be copied, modified, or distributed except according
 // to those terms.
 
+use anyhow::Context;
 use sequoia_openpgp as openpgp;
-use std::io;
+use std::{io, path::Path};
 
 use openpgp::{
     cert::prelude::ValidKeyAmalgamation,
-    packet::key::{PublicParts, UnspecifiedRole},
+    crypto::SessionKey,
+    packet::{
+        key::{PublicParts, UnspecifiedRole},
+        PKESK, SKESK,
+    },
+    parse::{
+        stream::{
+            DecryptionHelper, Decryptor, DecryptorBuilder, MessageStructure, VerificationHelper,
+        },
+        Parse,
+    },
     policy::Policy,
     serialize::stream::{Encryptor, LiteralWriter, Message, Recipient},
     types::{DataFormat, SymmetricAlgorithm},
@@ -33,6 +44,7 @@ pub fn parse_keyring<'a, K>(policy: &'a dyn Policy, keyring: K) -> io::Result<Ke
 where
     K: Iterator<Item = &'a Cert>,
 {
+    log::trace!("Searching certificates for data at rest encryption…");
     // get certificates from keyring
     let mut cert_list: Keyring = Vec::new();
     for cert in keyring {
@@ -50,7 +62,7 @@ where
             let algo = mpis.algo().unwrap();
             let size = mpis.bits().unwrap_or(0);
             log::info!(
-                "Encrypting for certificate {storage_cert} subkey {subkey} {algo}{size}",
+                "Encrypting for certificate {storage_cert} subkey {subkey} algo {algo}{size}",
                 storage_cert = storage_cert.to_string(),
                 subkey = subkey.to_string(),
                 algo = algo.to_string(),
@@ -78,7 +90,10 @@ where
     R: IntoIterator,
     R::Item: Into<Recipient<'a>>,
 {
-    log::trace!("Setting up encryption…");
+    log::trace!(
+        "Setting up encryption with {algo}…",
+        algo = SymmetricAlgorithm::AES256
+    );
     let message = Message::new(output);
     let encryptor =
         Encryptor::for_recipients(message, recipients).symmetric_algo(SymmetricAlgorithm::AES256);
@@ -93,4 +108,55 @@ where
         .format(DataFormat::Binary)
         .build()
         .map_err(openpgp_error)
+}
+
+pub struct Helper {}
+
+impl VerificationHelper for Helper {
+    fn get_certs(&mut self, _ids: &[openpgp::KeyHandle]) -> openpgp::Result<Vec<Cert>> {
+        Ok(Vec::new()) // Feed the Certs to the verifier here...
+    }
+
+    fn check(&mut self, _structure: MessageStructure) -> openpgp::Result<()> {
+        Ok(()) // Implement your verification policy here.
+    }
+}
+
+impl DecryptionHelper for Helper {
+    fn decrypt<D>(
+        &mut self,
+        _pkesks: &[PKESK],
+        skesks: &[SKESK],
+        _sym_algo: Option<SymmetricAlgorithm>,
+        mut decrypt: D,
+    ) -> openpgp::Result<Option<openpgp::Fingerprint>>
+    where
+        D: FnMut(SymmetricAlgorithm, &SessionKey) -> bool,
+    {
+        let password = &"streng geheim".into();
+        let session_key_result = skesks[0].decrypt(password);
+        let _ = session_key_result.map(|(algo, session_key)| decrypt(algo, &session_key));
+        Ok(None)
+    }
+}
+
+pub fn build_decryptor<'a, R: 'a + io::Read + Send + Sync>(
+    _private_key_store: &Path,
+    policy: &'a dyn Policy,
+    input: R,
+) -> openpgp::Result<Decryptor<'a, Helper>>
+where
+    R: IntoIterator,
+    R::Item: Into<Recipient<'a>>,
+{
+    // TODO feed private_key_store into Helper
+    let helper = Helper {};
+
+    log::trace!("Setting up decryption…");
+    let decryptor = DecryptorBuilder::from_reader(input)?
+        .mapping(false)
+        .with_policy(policy, None, helper)
+        .context("Decryption failed")?;
+
+    Ok(decryptor)
 }
