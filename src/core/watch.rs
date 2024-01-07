713eed4ab5f832624d24bf79e5ccfeecb3bb7547
Thomas Krennwallner <tk@postsubmeta.net>
2024-01-07 16:24:46 -0500

    core: add handler to Watch

--- src/core/watch.rs
+++ src/core/watch.rs
@@ -7,10 +7,13 @@
 // This file may not be copied, modified, or distributed except according
 // to those terms.
 
-use notify::{Event, RecommendedWatcher, Watcher};
+use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 use std::io;
+use std::path::PathBuf;
 use std::sync::mpsc::{Receiver, RecvError, SendError};
 use std::sync::{mpsc, Mutex};
+use tempfile::TempDir;
+use tokio::sync::mpsc::Sender;
 
 use super::notify::notify_error;
 
@@ -25,15 +28,27 @@ pub fn channel_recv_error(e: RecvError) -> io::Error {
 pub struct Watch {
     pub rx: Mutex<Receiver<notify::Result<Event>>>,
     pub watcher: RecommendedWatcher,
+    pub shutdown: TempDir,
+    _handler: Option<Sender<Option<PathBuf>>>,
 }
 
 impl Watch {
-    pub fn new() -> io::Result<Self> {
+    pub fn new(handler: Option<Sender<Option<PathBuf>>>) -> io::Result<Self> {
+        // here we can shutdown the watch
+        let shutdown = tempfile::tempdir()?;
         let (tx, rx) = mpsc::channel();
+        let mut watcher =
+            RecommendedWatcher::new(tx, notify::Config::default()).map_err(notify_error)?;
+        // let watcher monitor the shutdown path
+        watcher
+            .watch(shutdown.path(), RecursiveMode::NonRecursive)
+            .map_err(notify_error)?;
+
         Ok(Self {
             rx: Mutex::new(rx),
-            watcher: RecommendedWatcher::new(tx, notify::Config::default())
-                .map_err(notify_error)?,
+            watcher,
+            shutdown,
+            _handler: handler,
         })
     }
 }
